64383eea6c96ce7f565edb6693245b558161b904
Arthur FRIN <frin.arthur@gmail.com>
2026-06-19 20:28:46 +0200

    regreet

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -105,7 +105,7 @@
         enable = true;
         settings = {
           default_session = {
-            command = "${pkgs.cage}/bin/cage -s -- ${pkgs.greetd.regreet}/bin/regreet";
+            command = "${pkgs.cage}/bin/cage -s -- ${pkgs.regreet}/bin/regreet";
             user = "greeter";
           };
         };
